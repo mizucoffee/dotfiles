f5711dd20d7868ab540e5e1cb7f465629def5367
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-05-09 21:33:00 +0900

    [add] Add getgip/getlip file

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ curl -sL git.io/zplug | zsh
 
 sudo cp bin/wifi /usr/local/bin/wifi
 sudo cp bin/battery /usr/local/bin/battry
+sudo cp bin/getgip /usr/local/bin/getgip
+sudo cp bin/getlip /usr/local/bin/getlip
 
 sudo chmod 755 /usr/local/bin/wifi
 sudo chmod 755 /usr/local/bin/battery
+sudo chmod 755 /usr/local/bin/getgip
+sudo chmod 755 /usr/local/bin/getlip
 
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf

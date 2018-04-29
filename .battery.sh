e6d865e78d883380530b50ac822cf72ab18026ad
Ritsuki Goto <develop@mizucoffee.net>
2018-04-29 16:06:41 +0900

    [add] battery.sh

---
+++ .battery.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+vnstat -l -i wlp3s0 > ~/.speed&
+while true
+do
+  if [ `cat /sys/class/power_supply/AC/online` -eq 0 -a `cat /sys/class/power_supply/BAT0/capacity` -le 20 ] ; then
+    amixer set Capture toggle > /dev/null 2>&1
+  else
+    amixer set Capture cap > /dev/null 2>&1
+  fi
+  sleep 0.2
+done

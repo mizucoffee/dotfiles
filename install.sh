3ca45d697a1cce4928d6c17c64181bf8e2a2b003
Ritsuki Goto <develop@mizucoffee.net>
2018-05-02 15:19:54 +0900

    [fix] install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".config" ]] && continue
+    [[ "$f" == ".tmux.conf" ]] && continue
+    [[ "$f" == ".bar.sh" ]] && ps -e | grep i3 > /dev/null || continue
+    [[ "$f" == ".battery.sh" ]] && ps -e | grep i3 > /dev/null || continue
 
     ln -s -f `pwd`/$f ~
 done
 
-ln -s `pwd`/.config/i3/config ~/.config/i3/config
+ps -e | grep i3 > /dev/null && ln -s `pwd`/.config/i3/config ~/.config/i3/config
